61b46f467521918d7ce126de58833f816410181a
Álvaro Martínez-Cediel Algora <alvaro.martinez.algora@gmail.com>
2022-03-10 19:30:44 +0100

    3

--- Assets/Scripts/StartOptions.cs
+++ Assets/Scripts/StartOptions.cs
@@ -7,38 +7,36 @@ public class StartOptions : MonoBehaviour {
 
 
 
-	public int sceneToStart = 1;										//Index number in build settings of scene to load if changeScenes is true
-	public bool changeScenes;											//If true, load a new scene when Start is pressed, if false, fade out UI and continue in single scene
-	public bool changeMusicOnStart;										//Choose whether to continue playing menu music or start a new music clip
-	public int musicToChangeTo = 0;										//Array index in array MusicClips to change to if changeMusicOnStart is true.
+	public int sceneToStart = 1;										
+	public bool changeScenes;											
+	public bool changeMusicOnStart;										
+	public int musicToChangeTo = 0;										
 
 
-	[HideInInspector] public bool inMainMenu = true;					//If true, pause button disabled in main menu (Cancel in input manager, default escape key)
-	[HideInInspector] public Animator animColorFade; 					//Reference to animator which will fade to and from black when starting game.
-	[HideInInspector] public Animator animMenuAlpha;					//Reference to animator that will fade out alpha of MenuPanel canvas group
-	[HideInInspector] public AnimationClip fadeColorAnimationClip;		//Animation clip fading to color (black default) when changing scenes
-	[HideInInspector] public AnimationClip fadeAlphaAnimationClip;		//Animation clip fading out UI elements alpha
+	[HideInInspector] public bool inMainMenu = true;					
+	[HideInInspector] public Animator animColorFade; 					
+	[HideInInspector] public Animator animMenuAlpha;					
+	[HideInInspector] public AnimationClip fadeColorAnimationClip;		
+	[HideInInspector] public AnimationClip fadeAlphaAnimationClip;		
 
 
-	private PlayMusic playMusic;										//Reference to PlayMusic script
-	private float fastFadeIn = .01f;									//Very short fade time (10 milliseconds) to start playing music immediately without a click/glitch
-	private ShowPanels showPanels;										//Reference to ShowPanels script on UI GameObject, to show and hide panels
+	private PlayMusic playMusic;										
+	private float fastFadeIn = .01f;									
+	private ShowPanels showPanels;										
 
 	
 	void Awake()
 	{
-		//Get a reference to ShowPanels attached to UI object
+		//Referencia de ShowPanels
 		showPanels = GetComponent<ShowPanels> ();
 
-		//Get a reference to PlayMusic attached to UI object
+		//Referencia de PlayMusic
 		playMusic = GetComponent<PlayMusic> ();
 	}
 
 
 	public void StartButtonClicked()
-	{
-		//If changeMusicOnStart is true, fade out volume of music group of AudioMixer by calling FadeDown function of PlayMusic, using length of fadeColorAnimationClip as time. 
-		//To change fade time, change length of animation "FadeToColor"
+	{*
 		if (changeMusicOnStart) 
 		{
 			playMusic.FadeDown(fadeColorAnimationClip.length);
